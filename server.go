581107d45cc8efef7de463a4c29a029eee09b85a
Kostin Vladimir <vladimir.kostin@vitech.team>
2025-06-14 14:52:59 +0300

    добавлен метод получения user id

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -9,8 +11,19 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 )
+
+var guidNotFoundError = errors.New("в заголовке не найден Guid пользователя")
+var invalidGuidError = errors.New("в заголовке не верный Guid пользователя")
+
+const userGuidHeader = "X-User-Id"
+
+// ErrorResponse структура ошибки сервера.
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
 
 // NewServer создает новый инстанс роутера и сервера.
 func NewServer(port int, logLevel slog.Level) (*chi.Mux, *http.Server) {
@@ -39,4 +52,50 @@ func NewServer(port int, logLevel slog.Level) (*chi.Mux, *http.Server) {
 func AddRequestIDToRequestHeader(header http.Header, parentRequestID string) {
 	header.Set(traceParentHeader, parentRequestID)
 	header.Set(requestIdHeader, uuid.NewString())
+}
+
+// GetUserIDFromRequest получить id пользователя из заголовка
+func GetUserIDFromRequest(w http.ResponseWriter, r *http.Request) (uuid.UUID, bool) {
+	guid := r.Header.Get(userGuidHeader)
+	if guid == "" {
+		ErrorResponseOutput(r.Context(), w, guidNotFoundError, "")
+
+		return uuid.UUID{}, false
+	}
+
+	guidParsed, err := uuid.Parse(guid)
+	if err != nil {
+		ErrorResponseOutput(r.Context(), w, invalidGuidError, "")
+
+		return uuid.UUID{}, false
+	}
+
+	return guidParsed, true
+}
+
+// ErrorResponseOutput вывод ошибки запроса
+func ErrorResponseOutput(
+	ctx context.Context,
+	w http.ResponseWriter,
+	err error,
+	errMsg string,
+) {
+	if errors.Is(err, guidNotFoundError) || errors.Is(err, invalidGuidError) {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(ErrorResponse{Message: err.Error()})
+
+		return
+	}
+
+	if err != nil {
+		GetLogger(ctx).Error("ошибка запроса", "err", err)
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	_ = json.NewEncoder(w).Encode(ErrorResponse{Message: errMsg})
+}
+
+// SetUserIDToHeader добавить в заголовок ID пользователя
+func SetUserIDToHeader(header http.Header, userID uuid.UUID) {
+	header.Set(userGuidHeader, userID.String())
 }
